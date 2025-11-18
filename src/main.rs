c8076147e6c3908fe8b88e0c29b68d1ed2736586
solmidnight <sol@blockglow.com>
2025-11-17 19:00:53 -0600

    Another fix 3

--- src/main.rs
+++ src/main.rs
@@ -681,7 +681,7 @@ async fn run() -> anyhow::Result<()> {
             // Use HTTP/1 connection builder
             let service = service_fn(async |mut req| -> Result<_, anyhow::Error> {
                 use hyper::header::*;
-                req.headers_mut().append("X-Real-IP".into(), HeaderValue::new(peer_addr.as_str().to_string()));
+                req.headers_mut().append(HeaderName::from_str("X-Real-IP").unwrap(), HeaderValue::from_str(peer_addr.as_str()).unwrap());
                 Ok(handle_request(req, state.clone()).await)
             });
 
